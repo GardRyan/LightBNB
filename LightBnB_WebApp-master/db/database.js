06f04516240387703a1b31801108378cf1806661
GardRyan <res.gardner@gmail.com>
2024-03-10 13:11:02 -0400

    initial commit

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,18 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+import { Client } from 'pg'
+const client = new Client()
+ 
+client.connect((err) => {
+   client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+     console.log(err ? err.stack : res.rows[0].message) // Hello World!
+     client.end()
+   })
+})
+
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log(response)})
+ 
 /// Users
 
 /**
